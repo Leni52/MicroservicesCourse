7d137ceba57c05cd1395166319826e50a36189e6
Lenche Petrov <lenche.petrov@scalefocus.com>
2022-10-28 09:55:41 +0300

    initial setup

---
+++ PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,19 @@
+﻿using PlatformService.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PlatformService.Data
+{
+    public interface IPlatformRepo
+    {
+
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+
+        Platform GetPlatformById(int id);
+
+        void CreatePlatform(Platform platform);
+
+    }
+}
