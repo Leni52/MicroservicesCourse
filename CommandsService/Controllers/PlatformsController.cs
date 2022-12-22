cdf197ffe55d9ac80f39cc8973d51f7b7be82588
Lenche Petrov <lenche.petrov@scalefocus.com>
2022-12-22 13:15:24 +0200

    adding commandservice

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("---Inbound post");
+            return Ok();
+        }
+
+    }
+}
